95cd2a3dbb39d178f0d4dace2cd2f42845cb8b29
Anna <annadahacker@gmail.com>
2023-02-09 16:56:53 +0100

    remove shortform field from nummer

--- Nummer.java
+++ Nummer.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public abstract class Nummer {
     public Boolean isValid;
     public String stringForm;
-    public String shortForm;
     public String numberType;
     public String shapeRegex;
     public Boolean hasCorrectShape;
