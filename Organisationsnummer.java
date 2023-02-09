6fa877494500799d601085ffd70c8397e3eb9908
Anna <annadahacker@gmail.com>
2023-02-09 17:37:39 +0100

    update regex and add validity checks to be done

--- Organisationsnummer.java
+++ Organisationsnummer.java
@@ -1,11 +1,20 @@
 import java.util.ArrayList;
 
+/**
+ * Ett organisationsnummer används för juridiska personer (företag, föreningar,
+ * myndigheter osv). Första siffran anger vilken juridisk form organisationen har. Det
+ * mittersta sifferparet måste vara minst 20. Sista siffran är kontrollsiffra, precis som för
+ * personnummer. Ett organisationsnummer kan inleds med “århundrade” 16 om det
+ * anges på 12-siffrig form.
+ */
 public class Organisationsnummer extends Nummer {
     public Organisationsnummer(String inNumber) {
-        //this.shapeRegex = "^\\d{6}(?:\\d{2})?[[+][-]]?\\d{4}$"; TODO
+        this.shapeRegex = "^((16\\d{6})|(\\d{6}))[-]?\\d{4}$";
         this.numberType = "organisationsnummer";
         this.stringForm = inNumber;
         this.validityChecks = new ArrayList<ValidityCheck>();
         this.validityChecks.add(new ShapeCheck());
+        this.validityChecks.add(new ControlDigitCheck());
+        this.validityChecks.add(new MonthDigitCheck());
     }
 }
