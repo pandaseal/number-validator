7cfa25f015679ee27f63f93334e2e22e09ba9c2d
Anna <annadahacker@gmail.com>
2023-02-10 16:28:24 +0100

    implement month digit check

--- MonthDigitCheck.java
+++ MonthDigitCheck.java
@@ -5,7 +5,23 @@ public class MonthDigitCheck extends ValidityCheck {
 
     @Override
     public void performCheck(Nummer nummer) {
-        this.pass = false;
+        this.pass = monthDigitIsAtLeast20(nummer);
+    }
+
+    public boolean monthDigitIsAtLeast20(Nummer nummer) {
+        if (nummer.hasCorrectShape == null) {
+            nummer.hasCorrectShape = ShapeCheck.isCorrectShape(nummer);
+        }
+        if (!nummer.hasCorrectShape) {
+            this.failMessage = "Wrong shape, cannot check.";
+            return false;
+        }
+
+        if (nummer.getMonthDigits() >= 20) {
+            return true;
+        }
+        this.failMessage = "Month digit is not at least 20.";
+        return false;
     }
 
 }
