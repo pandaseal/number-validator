53b211e23289367d5359d7a3e592a662c1b1d852
Anna <annadahacker@gmail.com>
2023-02-10 16:28:53 +0100

    implement year digit check

---
+++ YearDigitCheck.java
@@ -0,0 +1,30 @@
+public class YearDigitCheck extends ValidityCheck {
+    public YearDigitCheck() {
+        this.nameOfCheck = "YearDigitCheck";
+    }
+
+    @Override
+    public void performCheck(Nummer nummer) {
+        this.pass = yearDigitIs16(nummer);
+    }
+
+    public boolean yearDigitIs16(Nummer nummer) {
+        if (nummer.hasCorrectShape == null) {
+            nummer.hasCorrectShape = ShapeCheck.isCorrectShape(nummer);
+        }
+        if (!nummer.hasCorrectShape) {
+            this.failMessage = "Wrong shape, cannot check.";
+            return false;
+        }
+        int yearDigits = nummer.getYearDigits();
+        if (yearDigits == -1) {
+            this.passMessage = "No year digit, cannot fail check.";
+            return true;
+        } else if (yearDigits == 16) {
+            return true;
+        } else {
+            this.failMessage = "Year digit is not 16.";
+            return false;
+        }
+    }
+}
