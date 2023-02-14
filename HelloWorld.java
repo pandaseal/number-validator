b5138660b6b903f6661350d919f32a06a6cb18af
Anna <annadahacker@gmail.com>
2023-02-14 17:00:03 +0100

    remove old java files

--- HelloWorld.java
+++
@@ -1,6 +0,0 @@
-// my first java program in a while
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- Second.java
+++
@@ -1,4 +0,0 @@
-public class Second {
-    public static void main(String[] args) {
-    }
-}
