d50aacbf9d19ace3043b1c87354b514df14757c2
Anna <annadahacker@gmail.com>
2023-02-16 17:43:30 +0100

    implement unit tests for ValidDateCheck

---
+++ app/src/test/java/numbervalidator/ValidDateCheckTest.java
@@ -0,0 +1,69 @@
+package numbervalidator;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for ValidDateCheck that is used for 
+ * Nummer objects of type Personnummer and Samordningsnummer.
+ */
+public class ValidDateCheckTest {
+    @Test
+    void isValidDateCheckPNTest() {
+        ValidDateCheck check = new ValidDateCheck();
+        assertTrue(check.isValidDate(new Personnummer("900118+9811")));
+        assertTrue(check.isValidDate(new Personnummer("201701102384")));
+        assertTrue(check.isValidDate(new Personnummer("141206-2380")));
+        assertTrue(check.isValidDate(new Personnummer("20080903-2386")));
+        assertTrue(check.isValidDate(new Personnummer("7101169295")));
+        assertTrue(check.isValidDate(new Personnummer("198107249289")));
+        assertTrue(check.isValidDate(new Personnummer("19021214-9819")));
+        assertTrue(check.isValidDate(new Personnummer("190910199827")));
+        assertTrue(check.isValidDate(new Personnummer("191006089807")));
+        assertTrue(check.isValidDate(new Personnummer("192109099180")));
+        assertTrue(check.isValidDate(new Personnummer("4607137454")));
+        assertTrue(check.isValidDate(new Personnummer("194510168885")));
+        assertTrue(check.isValidDate(new Personnummer("189102279800")));
+        assertTrue(check.isValidDate(new Personnummer("189912299816")));
+    }
+    
+    @Test
+    void isINValidDateCheckPNTest() {
+        ValidDateCheck check = new ValidDateCheck();
+        assertFalse(check.isValidDate(new Personnummer("190302299813")));
+    }
+
+    @Test
+    void isValidDateCheckSNTest() {
+        ValidDateCheck check = new ValidDateCheck();
+        assertTrue(check.isValidDate(new Samordningsnummer("190910799824")));
+        assertTrue(check.isValidDate(new Samordningsnummer("19091079-9824")));
+        assertTrue(check.isValidDate(new Samordningsnummer("0910799824")));
+        assertTrue(check.isValidDate(new Samordningsnummer("091079-9824")));
+    }
+    
+    @Test
+    void isINValidDateCheckSNTest() {
+        ValidDateCheck check = new ValidDateCheck();
+        assertFalse(check.isValidDate(new Samordningsnummer("")));
+        /* Lack of examples of Samordningsnummer. */
+    }
+
+    /* The following tests are disabled until someone has the time to implement them
+     * Until then, the above tests will do (they check the overall function).
+     */
+    @Disabled
+    @Test
+    void getRealDateTest() {}
+
+    @Disabled
+    @Test
+    void longDateExistsTest() {}
+
+    @Disabled
+    @Test
+    void shortDateExistsTest() {}
+}
